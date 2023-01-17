74f2c138527b454382d82f9f1b94a6e771a1270e
Reina_Sasiki <sasiki.64892@gmail.com>
2023-01-17 20:58:10 +0900

    initialize commit

---
+++ ClipMixer.cs
@@ -0,0 +1,117 @@
+﻿
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Rs.ClipMixer
+{
+    public class ClipMixer
+    {
+        public static void OverrideMixer(OverrideMixer cms)
+        {
+            var OverrideClipDict = ToTagPeaClips(FiltalingExtension(Directory.EnumerateFiles(cms.OverrideClipsPath), ".anim"));
+
+            Dictionary<string, AnimationClip> OutputAniClips = new Dictionary<string, AnimationClip>();
+
+            string OutputPath = cms.OutputPath + "/";
+
+            foreach (string SouseClipPath in FiltalingExtension(Directory.EnumerateFiles(cms.SourceClipsPath), ".anim"))
+            {
+                string SaveClipName = Path.GetFileName(SouseClipPath).Replace(TagListUp(SouseClipPath) + "_", string.Empty);
+
+                string SavePath = OutputPath + SaveClipName;
+
+                AnimationClip SaveClip = MixClipToTag(OverrideClipDict, TagListUp(SouseClipPath), AssetDatabase.LoadAssetAtPath<AnimationClip>(SouseClipPath));
+
+                OutputAniClips.Add(SavePath, SaveClip);
+            }
+
+            SaveAniClips(OutputAniClips);
+
+        }
+        public static IEnumerable<string> FiltalingExtension(IEnumerable<string> strings, string Extension)
+        {
+            List<string> Maths = new List<string>();
+
+            foreach (var str in strings)
+            {
+
+                if (Path.GetExtension(str) == Extension)
+                {
+                    Maths.Add(str);
+                }
+            }
+            return Maths;
+
+        }
+
+
+        public static void SaveAniClips(Dictionary<string, AnimationClip> SaveClips)
+        {
+            foreach (var kvp in SaveClips)
+            {
+                AssetDatabase.CreateAsset(kvp.Value, kvp.Key);
+            }
+        }
+
+
+
+        public static AnimationClip MixClipToTag(Dictionary<char, AnimationClip> OverideTagPeaClip, string SourceOvrrideTags, AnimationClip SourceClip)
+        {
+            var Aniclip = new AnimationClip();
+
+            foreach (char Tag in SourceOvrrideTags)
+            {
+                if (OverideTagPeaClip.ContainsKey(Tag))
+                {
+                    MixClip(ref Aniclip, OverideTagPeaClip[Tag]);
+                }
+                else
+                {
+                    Debug.LogWarning("タグ " + Tag + " が存在しません");
+                }
+            }
+
+            MixClip(ref Aniclip, SourceClip);
+
+            return Aniclip;
+        }
+
+        public static void MixClip(ref AnimationClip Base, AnimationClip mix)
+        {
+            var MixBinds = AnimationUtility.GetCurveBindings(mix);
+
+            foreach (var MixBind in MixBinds)
+            {
+                AnimationUtility.SetEditorCurve(Base, MixBind, AnimationUtility.GetEditorCurve(mix, MixBind));
+            }
+
+        }
+
+        public static string TagListUp(string path)
+        {
+            return Path.GetFileNameWithoutExtension(path).Split('_')[0];
+        }
+
+        public static Dictionary<char, AnimationClip> ToTagPeaClips(IEnumerable<string> ClipPaths)
+        {
+            Dictionary<char, AnimationClip> TagPeaClips = new Dictionary<char, AnimationClip>();
+
+            foreach (var ClipPath in ClipPaths)
+            {
+                var ClipName = Path.GetFileNameWithoutExtension(ClipPath);
+
+                var tag = TagListUp(ClipName);
+
+                if (tag.Length != 1) { Debug.LogWarning(ClipName + "の命名規則が正しくありません"); continue; }
+
+                TagPeaClips.Add(tag[0], AssetDatabase.LoadAssetAtPath<AnimationClip>(ClipPath));
+
+            }
+            return TagPeaClips;
+        }
+
+    }
+
+}
---
+++ OverrideMixer.cs
@@ -0,0 +1,90 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Rs.ClipMixer
+{
+
+    [CreateAssetMenu(fileName = "OverrideMixerSetting", menuName = "OverrideMixerSetting")]
+    public class OverrideMixer : ScriptableObject
+    {
+        public string SourceClipsPath;
+        public string OverrideClipsPath;
+        public string OutputPath;
+
+    }
+    [CustomEditor(typeof(OverrideMixer))]
+    public class OverrideMixerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+
+            OverrideMixer mytarget = target as OverrideMixer;
+
+            var MyPath = AssetDatabase.GetAssetPath(mytarget);
+
+            var ParentPath = new DirectoryInfo(MyPath).Parent.FullName;
+
+            var SourceClipsPathProp = serializedObject.FindProperty("SourceClipsPath");
+
+            EditorGUILayout.LabelField("SourceClipsPath   " + SourceClipsPathProp.stringValue);
+
+            if (GUILayout.Button("SourceClipsPath Select"))
+            {
+                var getpaht = FileUtil.GetProjectRelativePath(EditorUtility.OpenFolderPanel("Select SourceClipsPath", ParentPath, ""));
+
+                if (!string.IsNullOrEmpty(getpaht))
+                {
+                    SourceClipsPathProp.stringValue = getpaht;
+                    serializedObject.ApplyModifiedProperties();
+                }
+            }
+
+            var OverrideClipsPathProp = serializedObject.FindProperty("OverrideClipsPath");
+
+            EditorGUILayout.LabelField("OverrideClipsPath   " + OverrideClipsPathProp.stringValue);
+
+            if (GUILayout.Button("OverrideClipsPath Select"))
+            {
+                var getpaht = FileUtil.GetProjectRelativePath(EditorUtility.OpenFolderPanel("Select OverrideClipsPath", ParentPath, ""));
+
+                if (!string.IsNullOrEmpty(getpaht)) 
+                {
+                    OverrideClipsPathProp.stringValue = getpaht;
+                    serializedObject.ApplyModifiedProperties();
+                }
+            }
+
+            var OutputPathProp = serializedObject.FindProperty("OutputPath");
+
+            EditorGUILayout.LabelField("OutputPath   " + OutputPathProp.stringValue);
+
+            if (GUILayout.Button("OutputPath Select"))
+            {
+                var getpaht = FileUtil.GetProjectRelativePath(EditorUtility.OpenFolderPanel("Select OutputPath", ParentPath, ""));
+
+                if (!string.IsNullOrEmpty(getpaht))
+                {
+                    OutputPathProp.stringValue = getpaht;
+                    serializedObject.ApplyModifiedProperties();
+                }
+            }
+
+
+            GUILayout.Label("");
+
+            if (GUILayout.Button("ClipMix!!"))
+            {
+                ClipMixer.OverrideMixer(mytarget);
+            }
+
+
+
+
+        }
+
+    }
+}
